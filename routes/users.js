cfeea8cd1f3f384be441f4ac8440435a3a5e3a59
Mau <is716122@iteso.mx>
2019-12-04 09:53:40 -0600

    Se agrega el DELETE para el endpoint usuarios

--- routes/users.js
+++ routes/users.js
@@ -4,57 +4,55 @@ const User = require('../db/users');
 
 router.route('/')
     .get((req, res) => {
-        if(req.esAdmin){
-            User.find({}, (err, docs) => {
-                if(err) {
-                    res.statusCode = 500;
-                    res.end();
-                }
-                else {
-                    res.statusCode = 200;
-                    res.send(docs);
-                }
-            });
-        } else{
-            res.statusCode = 401;
-            res.end();
-        }
+        User.find({}, (err, docs) => {
+            if (err) {
+                res.statusCode = 500;
+                res.end();
+            } else {
+                res.statusCode = 200;
+                res.send(docs);
+            }
+        });
 
     })
     .post(async function (req, res) {
         let newUser = req.body;
 
         // Validar si vienen las propiedades
-        if(!newUser.nombre || !newUser.apellido || !newUser.password || !newUser.expediente || !newUser.carrera || !newUser.email) {
+        if (!newUser.nombre || !newUser.apellido || !newUser.password || !newUser.expediente || !newUser.carrera || !newUser.email) {
             res.statusCode = 400;
             res.send('Las propiedades requeridas son: Nombre, Apellido, Password, Expediente, Carrera y Correo ');
-        }
-        else {
+        } else {
             // Validar si existe un usuario con el mismo correo o nombres y apellidos
-            let sameEmailUser = await User.find({email: newUser.email});
-            let sameNameUser = await User.find({nombre: newUser.nombre, apellido: newUser.apellido});
+            let sameEmailUser = await User.find({
+                email: newUser.email
+            });
+            let sameNameUser = await User.find({
+                nombre: newUser.nombre,
+                apellido: newUser.apellido
+            });
 
-            if(sameEmailUser.length > 0) {
+            if (sameEmailUser.length > 0) {
                 res.statusCode = 400;
                 res.send('Ya existe un usuario con el mismo correo');
-            }
-            else if(sameNameUser.length > 0) {
+            } else if (sameNameUser.length > 0) {
                 res.statusCode = 400;
                 res.send('Ya existe un usuario con el mismo nombre');
-            }
-            else {
+            } else {
                 newUser.rol = "Estudiante";
                 newUser.numReviews = 0;
                 let max = 0;
 
-                let user = await User.findOne().sort({id: -1});
+                let user = await User.findOne().sort({
+                    id: -1
+                });
 
-                if(user!=null){
+                if (user != null) {
                     max = user.id;
                 }
 
 
-            newUser.id = max+1;
+                newUser.id = max + 1;
 
                 let userDocument = User(newUser);
                 userDocument.save()
@@ -65,9 +63,33 @@ router.route('/')
                     .catch(reason => {
                         res.statusCode = 500;
                         res.end();
-                    });       
+                    });
             }
         }
     });
+
+
+
+router.route('/:id')
+
+.get((req, res) => {
+    User.findOne({id: req.params.id}, (err, doc) => {
+        if (err) {
+            res.statusCode = 500;
+            res.end();
+        } else {
+            res.statusCode = 200;
+            res.send(doc);
+        }
+    });
+}).delete(async (req,res)=>{
+    await User.findOneAndDelete({id: req.params.id});
+
+    req.statusCode = 200;
+    res.end();
+});
+
+
+;
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,59 +1,20 @@
 const express = require('express');
-const randomize = require('randomatic');
 const port = 3000;
 const app = express();
 const usersRouter = require('./routes/users');
-const productsRouter = require('./routes/products');
+const loginRouter = require('./routes/login');
 const User = require('./db/users')
 const Token = require('./db/token')
 // const mongo = require('./db/mongodb-connect');
 
 app.use(express.json());
 
-app.post('*', (req, res, next) => {
-    console.log('POST happen')
-    next();
-  })
-
-
 app.use('/api/users', authMiddleware);
 app.use('/api/users', authUsr);
 
-
+app.use('/api/login', loginRouter);
 app.use('/api/users', usersRouter);
 
-
-
-
-
-
-
-app.post('/api/login', function (req, res) {
-    // Programar aquí lógica de token
-    User.find({email: req.body.email, password: req.body.password})
-        .then(async users => {
-            if(users.length > 0) {
-                let user = users[0];
-                let tokenString = randomize('Aa0','10')+'-'+user.id;
-
-                await Token.findOneAndDelete({userId: user.id});
-                let tokenDoc = Token({userId: parseInt(user.id), token: tokenString});
-                await tokenDoc.save();
-
-                res.statusCode = 200;
-                res.send({token: tokenString});
-            }
-            else {
-                res.statusCode = 400;
-                res.end();
-            }
-        })
-        .catch(reason => {
-            res.statusCode = 500;
-            res.end();
-        });
-    
-});
 
 
 app.listen(port, () => {
