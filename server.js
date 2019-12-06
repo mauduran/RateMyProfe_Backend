347f12610429ac2138419b210546ba25b4bb2ea3
Mau <is716122@iteso.mx>
2019-12-06 00:25:08 -0600

    se cambia puerto del server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 // const cors = require('cors');
 const app = express();
 const usersRouter = require('./routes/users');
